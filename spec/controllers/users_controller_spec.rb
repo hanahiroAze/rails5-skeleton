f69462084dc382063962f05565eeb2b6fbcdb519
hanahiroAze <hiroyuki.hanai@gmail.com>
2017-03-16 14:08:41 +0900

    add update index show test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,24 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:test_user_param) do
+    {
+        params:{
+            user: {
+                name: 'test',
+                email: 'test@testtest.com',
+                password: 'rails5',
+                password_confirmation: 'rails5'
+            }
+        }
+    }
+  end
 
   describe "POST success User::create" do
-    let(:test_user_param) do
-      {
-        params:{
-          user: {
-            name: 'test',
-            email: 'test@testtest.com',
-            password: 'rails5',
-            password_confirmation: 'rails5'
-          }
-        }
-      }
-    end
     it 'create user' do
       expect {
         post :create, test_user_param
       }.to change(User, :count).by(1)
+    end
+  end
+
+  describe 'After Login' do
+    before do
+      post :create, test_user_param
+      login User.first
+    end
+    it 'create user' do
+      get :edit, {:params => {:id => User.first.id} }
+      expect(response).to have_http_status(200)
     end
   end
 end
--- spec/support/spec_test_helper.rb
+++ spec/support/spec_test_helper.rb
@@ -5,6 +5,15 @@ module SpecTestHelper
       session.store(k, v)
     }
   end
+
+  def login(user)
+    user = User.where(:login => user.to_s).first if user.is_a?(Symbol)
+    request.session[:user] = user.id
+  end
+
+  def current_user
+    User.find(request.session[:user])
+  end
 end
 
 RSpec.configure do |config|
