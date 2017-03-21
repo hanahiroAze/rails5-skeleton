9112850096369512278e454331368a00f51117a1
hanahiroAze <hiroyuki.hanai@gmail.com>
2017-03-21 17:27:47 +0900

    add order by

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -4,16 +4,34 @@ RSpec.describe Micropost, type: :model do
   before do
     User.with_writable {User.create(name:'aaab', email:'aaa@gmail.xx', password: "foobar", password_confirmation: "foobar")}
     @user = User.first
-    @micropost = Micropost.new(content: 'Lorem ipsum', user_id: @user.id)
+    @micropost = @user.microposts.build(content: 'Lorem ipsum')
   end
 
   describe 'parameters' do
     it 'valid micropost' do
       expect(@micropost.valid?).to eq(true)
     end
+
     it 'invalid micropost' do
       @micropost.user_id = nil
       expect(@micropost.valid?).to eq(false)
+    end
+
+    it 'invalid micropost content blank' do
+      @micropost.content = ' '
+      expect(@micropost.valid?).to eq(false)
+    end
+
+    it 'invalid micropost content too long' do
+      @micropost.content = 'a' * 141
+      expect(@micropost.valid?).to eq(false)
+    end
+  end
+
+  describe 'orders' do
+    fixtures :microposts
+    it 'should be most recent first' do
+      expect(microposts(:most_recent)).to eq(Micropost.first)
     end
   end
 end
